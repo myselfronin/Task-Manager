aaac8fe6bb74bc2718b8b1a08fb9006963cc024c
Rabinson Ghatani <myself.ronin@hotmail.com>
2019-11-13 08:18:19 +0545

    Created Task Model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -14,13 +14,33 @@ const User = mongoose.model('User',{
     }
 });
 
-const me = new User({
-    name: 'Rabin',
-    age: 22
+// const me = new User({
+//     name: 'Rabin',
+//     age: 22
+// });
+//
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('Error!! :' , error);
+// });
+
+const Task = mongoose.model('Task',{
+    description: {
+        type: String,
+    },
+    completed: {
+        type: Boolean,
+    }
 });
 
-me.save().then(() => {
-    console.log(me);
+const task = new Task({
+    description: 'Learn the Mongoose library',
+    completed: false
+});
+
+task.save().then(() => {
+    console.log(task);
 }).catch((error) => {
-    console.log('Error!! :' , error);
+    console.log(error);
 });
