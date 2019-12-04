7a2a2031a0d38e91e842783b1bd46865d8c9e8d7
Rabinson Ghatani <myself.ronin@hotmail.com>
2019-12-04 10:21:22 +0545

    Promise chaining concepts

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5dcb6b133068e7498f446a49').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5dcb6b133068e7498f446a49').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// });
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count;
+};
+
+deleteTaskAndCount('5dd20730ccdb46507a70541d').then((count) => {
+    console.log(count);
 }).catch((e) => {
     console.log(e)
 });
