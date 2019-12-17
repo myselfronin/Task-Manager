ac5a9523309390dcbfffb0aa375876d74298da49
Rabinson Ghatani <myself.ronin@hotmail.com>
2019-12-17 20:16:38 +0545

    Added authentication and cascade delete

--- src/index.js
+++ src/index.js
@@ -7,33 +7,21 @@ const taskRouter = require('./routes/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disable')
-//     } else {
-//         next();
-//     }
-// });
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!');
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-// const jwt = require('jsonwebtoken');
-//
-// const myFunction = async () => {
-//     const token = jwt.sign( { _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days'});
-//     console.log(token);
-//
-//     const data =jwt.verify(token, 'thisismynewcourse');
-//     console.log(data);
-// };
-// myFunction();
+const User = require('./models/user');
+const main = async  () => {
+    // const task = await Task.findById('5df6f034b9a41946d1e497c3');
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
 
+    const user = await User.findById('5df6ef1a7c09b92e87cd57ba');
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks);
+};
+main()
 app.listen(port, () => {
     console.log('Server is up on port' + port)
 });
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -49,6 +50,12 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner',
+});
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
     const token = jwt.sign({ _id: user._id.toString() }, '#taskmanagerusertoken[]');
@@ -90,6 +97,15 @@ userSchema.pre('save', async function (next) {
     if(user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8);
     }
+
+    next();
+});
+
+//Delete user tasks when user is remove
+userSchema.pre('remove', async function (next) {
+    const user = this;
+
+    await Task.deleteMany({ owner:user._id });
 
     next();
 });
