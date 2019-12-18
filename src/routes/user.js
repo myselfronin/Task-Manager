75f69f72a4ba737d498b550e0b60e7c487b07799
Rabinson Ghatani <myself.ronin@hotmail.com>
2019-12-18 20:55:29 +0545

    Added avatar upload URI

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
+const multer = require('multer');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
@@ -79,6 +80,28 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch(e) {
         res.status(500).send(e);
     }
+});
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000, //Max. 1MB file can be uploaded
+    },
+    fileFilter (req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'));
+        }
+
+        cb(undefined, true);
+    }
+
+});
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 });
 
 module.exports = router;
